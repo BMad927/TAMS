84a4b0a32a9d64b8a1931d2efa6740317d38bc57
Melly Andrew Borja <borja0927@gmail.com>
2025-09-04 02:56:59 +0800

    update Script to use Contacts and Apply Form

--- script.js
+++ script.js
@@ -38,13 +38,12 @@ renderAnalytics();
 // ---------------------------
 // Supabase Setup
 // ---------------------------
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://frmsjykcwzklqzaxfiyq.supabase.co";    
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZybXNqeWtjd3prbHF6YXhmaXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MDgzMjEsImV4cCI6MjA3MjQ4NDMyMX0.v7pwM3qU8RzHKe0RYuMq0hSG95sKzwLH4LYCRvZyFNo";
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseUrl = 'https://frmsjykcwzklqzaxfiyq.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 // ---------------------------
 // Contact Form Handler
