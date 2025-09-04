6f528b514bfb16c5d8e56065b1ca0b857c8787b2
Melly Andrew Borja <borja0927@gmail.com>
2025-09-05 01:20:49 +0800

    update css

--- api/analytics.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    revenue: [10000, 12000, 15000, 18000], // yearly revenue
-    ticketsHandled: [250, 300, 400, 500]   // yearly tickets
-  });
-}
--- api/users.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    users: [
-      { id: 1, name: "Melly Borja", role: "Owner" },
-      { id: 2, name: "Matt Paolo", role: "Engineer" },
-      { id: 3, name: "Ethan", role: "Technician" }
-    ]
-  });
-}
